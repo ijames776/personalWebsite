fccb54b4cd8e6e891fc82da83f65af4985eef4ab
iJames <ian.james0511@gmail.com>
2020-11-30 15:51:30 -0500

    Updated about page

--- about.php
+++ about.php
@@ -1,1 +1,21 @@
 <?php include 'includes/header.php' ?>
+
+<div class="container">
+    <div class="col">
+        <br>
+        <div style="background-color: rgba(255, 255, 255, .85)" class="row rounded shadow p-3">
+            <p>
+                I am an aspiring software developer who has been working since 2017 to break into the industry. I am particularly strong in C++ but have also spent significant time with SQL, HTML/CSS, PHP, and Javascript, along with the Boostrap Framework for web development. I use github for version control on all my projects. While it hasn't been a primary focus, I've also worked in Java and Python. I love programming. I work a work full-time job 40 hours every week, and then come home and code for several hours every night, and more time on the weekends. What drives me is the thrill that comes from setting a challenging goal or doing something for the first time, and then seeing it work after hours, days, or more of working through setbacks, failures, and bugs. It's an amazing feeling and I'm chasing it every day.
+            </p>
+            <p>
+                Most of my initial exposure to programming came from courses on udemy.com that provided a great way to learn languages on my own time at my own pace. I still turn to Udemy to learn new skills and am working through a vue.js course right now. However, I'm really a kinesthetic learner and so I very quickly jumped in and started learning by doing. I set up ambitious personal projects and had to find ways to solve problems, connect code together, and deal with the frustration of having to refactor large blocks of old code after learning better ways to do things. That approach, as frustrating as it was at times, taught me so much, and I can't recommend it enough.
+            </p>
+            <p>
+                After developing a foundation in C++, I enrolled in the Computer Programming Masters Certificate program at Harvard University. That program was designed to give non CS majors who already have a Bachelor's Degree to develop thefoundation they missed out. I learned Java in a month in order to prepare for those classes, and then proceeded to study algorithms, recursion, data structures, database systems, and a number of other topics. The formal curriculum and structure pushed me to use the fundamentals I had picked up from Udemy in ways I hadn't considered, exposed me to new challenges, languages, frameworks, and systems.
+            </p>
+            <p>
+                Now, with that degree in hand, and more than 1,000 hours spent coding on my own, I am ready to code professionally.
+            </p>
+        </div>
+    </div>
+</div>
--- contact.php
+++ contact.php
@@ -3,3 +3,10 @@
 
 ?>
 
+<div class="container">
+    <div class="col">
+        <br>
+        <div style="background-color: rgba(255, 255, 255, .85)" class="row rounded shadow p-3">
+        </div>
+    </div>
+</div>
