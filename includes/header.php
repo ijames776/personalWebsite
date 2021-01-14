1c2305eed87b8e0ecfee2eaefe3754cf05081de9
iJames <ian.james0511@gmail.com>
2021-01-14 09:48:12 -0500

    Updated main font

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,10 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
 </head>
